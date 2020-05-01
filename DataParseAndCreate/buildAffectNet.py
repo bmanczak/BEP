6ea70c53af4c4f37a6eba15127d65ebfe315048e
Blazej Manczak <manczakb@gmail.com>
2020-05-01 08:52:12 +0200

    Scripts for parsing and preprocessing the data
    Preprocessing.py contains a preprocessing class that is responsible for CLAHE image normalization, face detection and landmark extraction using MTCNN detector, face alignment and extraction
    
    buildRAF.py and buildAffectNet.py parse the directory structure in order to create compressed, preprocessed data representation .npz file format. These files are later used for training the model.

---
+++ DataParseAndCreate/buildAffectNet.py
@@ -0,0 +1,165 @@
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+import numpy as np
+import pandas as pd
+import keras
+import os
+import time
+from Preprocessing import *
+from mtcnn.mtcnn import MTCNN  # face detector, install with pip install mtcnn
+import cv2
+import argparse
+
+
+def getListOfFiles(dirName):
+    # create a list of file and sub directories
+    # names in the given directory
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    # Iterate over all the entries
+    for entry in listOfFile:
+        # Create full path
+        fullPath = os.path.join(dirName, entry)
+        # If entry is a directory then get the list of files in this directory
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+
+    return allFiles
+
+
+def read_data(input_dir,
+              output_dir,
+              prepro,
+              start_ind,
+              end_ind):
+
+    # """
+    validation_csv = pd.read_csv('/mnt/server-home/TUE/20175985/BepDataResNet/ManuallyAnotatedFileList/validation.csv', sep=',', names=["File_path", 'Face_x', 'Face-y', "Face_width", "Face_height",
+                                                                                                                                        'Facial_landmarks', 'Expression', 'Valence', 'Arousal'], index_col="File_path")
+
+    training_csv = pd.read_csv('/mnt/server-home/TUE/20175985/BepDataResNet/ManuallyAnotatedFileList/training.csv', sep=',', names=["File_path", 'Face_x', 'Face-y', "Face_width", "Face_height",
+                                                                                                                                    'Facial_landmarks', 'Expression', 'Valence', 'Arousal'], index_col="File_path", header=1)
+
+    # """
+    training_csv['Expression'] = pd.to_numeric(training_csv['Expression'])
+    validation_csv['Expression'] = pd.to_numeric(validation_csv['Expression'])
+
+    training_csv = training_csv[training_csv['Expression'].isin([0, 1, 2, 3, 4, 5, 6])]
+    validation_csv = validation_csv[validation_csv['Expression'].isin([0, 1, 2, 3, 4, 5, 6])]
+
+    train_and_val = pd.concat([training_csv, validation_csv])
+    train_and_val['RowNumber'] = range(1, train_and_val.shape[0]+1)
+
+    # train_and_val = train_and_val[train_and_val['Expression'].isin(
+    #    [0, 1, 2, 3, 4, 5, 6])]
+
+    found_image_paths = getListOfFiles(input_dir)
+    print("Found this many image paths:", len(found_image_paths))
+    X_train = []
+    Y_train = []
+
+    X_test = []
+    Y_test = []
+
+    failures = 0  # keeping track of failed imports
+    failures_prepro = 0
+    class_count = dict(zip(range(0, 7), [0]*7))  # keep track of label counts
+    if prepro == True:
+        detector = MTCNN()
+    start = time.time()
+    for path in found_image_paths[start_ind: end_ind]:
+
+        try:  # image is avaliable
+
+            #print("Checking path", path)
+            path_for_csv = "/".join(path.split("/")[-2:])
+            label_and_rowNum = train_and_val.loc[path_for_csv, ["Expression", "RowNumber"]]
+            #print("Path matched to csv")
+            img = load_img(path)
+            #print("Image loaded")
+            x = img_to_array(img).astype(np.uint8)
+            #print("Image to array")
+
+            if prepro == True:
+                try:
+                    x = applyPreprocessing(img_array=x, detector=detector)
+                except:
+                    failures_prepro += 1
+
+            x = cv2.resize(x, (224, 224))
+
+            # faster way of establishing whether row comes from validation set
+            if label_and_rowNum[1] > training_csv.shape[0]:
+                X_test.append(x)
+                Y_test.append(label_and_rowNum[0])
+            else:
+                X_train.append(x)
+                Y_train.append(label_and_rowNum[0])
+
+            class_count[label_and_rowNum[0]] += 1  # keep track of label counts
+            #print("Image has been processed with path", path)
+        except:
+            #print("Failed path", path)
+            failures += 1
+
+    X_train = np.asarray(X_train).astype('float16') / 255.  # scaling the images by 255
+    Y_train = keras.utils.to_categorical(Y_train, num_classes=7)
+
+    X_test = np.asarray(X_test).astype('float16') / 255.  # scaling the images by 255
+    Y_test = keras.utils.to_categorical(Y_test, num_classes=7)
+
+    np.savez_compressed(
+        os.path.join("/mnt/server-home/TUE/20175985/BepDataResNet/npzData/", "trainDataProcessed7Classes" + str(start_ind) + "To" + str(end_ind)), X_train=X_train, Y_train=Y_train)
+    np.savez_compressed(
+        os.path.join("/mnt/server-home/TUE/20175985/BepDataResNet/npzData/", "testDataProcessed7Classes" + str(start_ind) + "To" + str(end_ind)), X_test=X_test, Y_test=Y_test)
+
+    print("There have been {} failures".format(failures))
+    print("There have been {} failures in preprocessing".format(failures_prepro))
+
+    sum_all_vals = sum(class_count.values())
+    for key in class_count.keys():
+        class_count[key] = round(class_count[key] / sum_all_vals, 2)
+
+    print("Test Data size", X_test.shape, Y_test.shape)
+    print("Train Data size", X_train.shape, Y_train.shape)
+
+    print("The class distirbution is:", class_count)
+    print("The function took", round(time.time() - start, 2), "seconds")
+
+
+if __name__ == "__main__":
+    print("Mains")
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--input-dir",
+                        required=False,
+                        type=str,
+                        default="/mnt/server-home/TUE/20175985/BepDataResNet/ManuallyAnnotated",
+                        help='Directory of images')
+
+    parser.add_argument("--output-dir",
+                        required=False,
+                        type=str,
+                        default="/mnt/server-home/TUE/20175985/BepDataResNet/npzData",
+                        help='Directory of to save to ')
+
+    parser.add_argument("--start-ind",
+                        required=True,
+                        type=int,
+                        help='Index to start')
+
+    parser.add_argument("--end-ind",
+                        required=True,
+                        type=int,
+                        help='Index to finish')
+
+    parser.add_argument("--prepro",
+                        required=False,
+                        type=bool,
+                        default=True,
+                        help="Do preprocessing?")
+
+    parse_args, unknown = parser.parse_known_args()
+
+    read_data(**parse_args.__dict__)
---
+++ DataParseAndCreate/buildRAF.py
@@ -0,0 +1,150 @@
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+import numpy as np
+import pandas as pd
+import keras
+import os
+import time
+from Preprocessing import *
+from mtcnn.mtcnn import MTCNN  # face detector, install with pip install mtcnn
+import cv2
+import argparse
+
+
+def getListOfFiles(dirName):
+    """ create a list of files and sub directories """
+    # names in the given directory
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    # Iterate over all the entries
+    for entry in listOfFile:
+        # Create full path
+        fullPath = os.path.join(dirName, entry)
+        # If entry is a directory then get the list of files in this directory
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+
+    return allFiles
+
+
+def read_data(img_path,
+              txt_path,
+              path_bounding_box,
+              output_dir,
+              prepro):
+
+    data = pd.read_csv(txt_path, sep=' ', header=None)
+    data.columns = ["Path", "Expression"]
+    data['Status'] = data['Path'].apply(lambda row: row.split("_")[0])
+    data['FileName'] = data['Path'].apply(lambda row: row.split(".")[0])
+
+    rafToAffect = {1: 3, 2: 4, 3: 5, 4: 1, 5: 2, 6: 6, 7: 0}
+    data['Expression'] = data['Expression'].apply(lambda row: rafToAffect[row])
+    data['Path'] = data['Path'].apply(lambda row: os.path.join(img_path, row))
+    data.set_index('Path', drop=True, inplace=True)
+
+    X_train = []
+    Y_train = []
+
+    X_test = []
+    Y_test = []
+
+    failures = 0  # keeping track of failed imports
+    class_count = dict(zip(range(0, 7), [0]*7))
+    detector = MTCNN()
+
+    start = time.time()
+
+    for path in data.index:
+        try:
+            # print(path)
+            img = load_img(path)
+            x = img_to_array(img).astype(np.uint8)
+
+            if prepro == True:
+                try:
+                    x = applyPreprocessing(img_array=x, detector=detector)
+                    #print("Processed path ", path)
+                except:
+                    box = open(os.path.join(path_bounding_box,
+                                            data.loc[path, "FileName"] + "_boundingbox.txt"), "r")
+                    (X, Y, W, H) = [round(float(i)) for i in box.read().split(" ")[:-1]]
+                    x = x[Y:Y+H, X:X+W]
+                    #print("failed preprocessing MTCNN. Sucess with bounding box")
+
+                x = cv2.resize(x, (224, 224))
+
+            if data.loc[path, 'Status'] == 'test':
+                X_test.append(x)
+                Y_test.append(data.loc[path, "Expression"])
+            else:
+                X_train.append(x)
+                Y_train.append(data.loc[path, "Expression"])
+
+            class_count[data.loc[path, "Expression"]] += 1  # keep track of label counts
+        except:
+            print("failed for path", path)
+            failures += 1
+
+    X_train = np.asarray(X_train).astype('float16') / 255.  # scaling the images by 255
+    Y_train = keras.utils.to_categorical(Y_train, num_classes=7)
+
+    X_test = np.asarray(X_test).astype('float16') / 255.  # scaling the images by 255
+    Y_test = keras.utils.to_categorical(Y_test, num_classes=7)
+
+    np.savez_compressed(
+        os.path.join(output_dir, "trainDataProcessed"), X_train=X_train, Y_train=Y_train)
+    np.savez_compressed(
+        os.path.join(output_dir, "testDataProcessed"), X_test=X_test, Y_test=Y_test)
+
+    print("There have been {} failures".format(failures))
+
+    sum_all_vals = sum(class_count.values())
+    for key in class_count.keys():
+        class_count[key] = round(class_count[key] / sum_all_vals, 2)
+
+    print("Test Data size", X_test.shape, Y_test.shape)
+    print("Train Data size", X_train.shape, Y_train.shape)
+
+    print("The class distirbution is:", class_count)
+    print("The function took", round(time.time() - start, 2), "seconds")
+
+
+if __name__ == "__main__":
+    print("Mains")
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--img-path",
+                        required=False,
+                        type=str,
+                        default="/mnt/server-home/TUE/20175985/basicRaf/Image/original",
+                        help='Directory of images')
+
+    parser.add_argument("--txt-path",
+                        required=False,
+                        type=str,
+                        default="/mnt/server-home/TUE/20175985/basicRaf/EmoLabel/list_patition_label.txt",
+                        help='Directory of txt file')
+
+    parser.add_argument("--path_bounding_box",
+                        required=False,
+                        type=str,
+                        default="/mnt/server-home/TUE/20175985/basicRaf/Annotation/boundingbox",
+                        help="Directory of a file with bounding box coordinates")
+
+    parser.add_argument("--output-dir",
+                        required=False,
+                        type=str,
+                        default="/mnt/server-home/TUE/20175985/basicRaf/dataNpz",
+                        help="Where to save the data")
+
+    parser.add_argument("--prepro",
+                        required=False,
+                        type=bool,
+                        default=True,
+                        help="Do preprocessing?")
+
+    parse_args, unknown = parser.parse_known_args()
+
+    read_data(**parse_args.__dict__)
